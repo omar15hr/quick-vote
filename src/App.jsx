6c200db66d692038814a2aa35c0d7a2a580f8742
omar <omarhrn@gmail.com>
2024-12-28 10:53:20 -0300

    configurar socket en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react"
 import { PersonAdd } from "./components/PersonAdd"
 import { PersonList } from "./components/PersonList"
+import { io } from "socket.io-client"
+import { useEffect } from "react"
 
+
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports: ['websocket'],
+  });
+  return socket;
+}
 
 function App() {
+
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    console.log(socket)
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true);
+    })
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    })
+  }, [socket]);
 
   return (
     <div className="container">
       <div className="alert">
         <p>
           Service Status:
-          <span className="text-success"> Online</span>
-          <span className="text-danger"> Offline</span>
+          {
+            online
+              ? <span className="text-success"> Online</span>
+              : <span className="text-danger"> Offline</span>
+          }
         </p>
       </div>
 
